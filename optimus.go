e5e3db7a37755dda5ca94ca685b77eb9b8b5f830
aQaTL <mmsoltys@outlook.com>
2018-02-22 23:29:15 +0100

    cgo flags corrected

--- optimus.go
+++ optimus.go
@@ -1,7 +1,7 @@
 package nvidiaoptimus
 
 /*
-#cgo LDFLAGS: ${SRCDIR}/flags.def
+#cgo LDFLAGS: -L${SRCDIR}/flags.def
 
 __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
 __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
